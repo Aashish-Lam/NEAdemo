2f2a5fcfcef6a6baab37a508a74fd3e0a772619f
Aashish-Lam <aashishlamichhane@kbc.edu.np>
2024-06-27 14:37:26 +0545

    Fix the services links for both mobile and desktop

--- src/component/Service.jsx
+++ src/component/Service.jsx
@@ -6,7 +6,6 @@ import {
   FaComment,
   FaDove,
   FaFeatherAlt,
-  FaGlobeAsia,
   FaHandHoldingMedical,
   FaIdCard,
   FaLaptop,
@@ -16,163 +15,156 @@ import { useTranslation } from "react-i18next";
 
 const IconTile = () => {
   const { t } = useTranslation();
+
+  const smallScreenIcons = [
+    {
+      Icon: FaIdCard,
+      title: "attendance",
+      description: "manageAttendance",
+      link: "https://nea.org.np/hajiri",
+    },
+    {
+      Icon: FaMoneyBill,
+      title: "payroll",
+      description: "Neapayroll",
+      link: "https://nea.org.np/empPayroll",
+    },
+    {
+      Icon: FaCarAlt,
+      title: "vehicle",
+      description: "vehicleManagement",
+      link: "https://nea.org.np/vehicle",
+    },
+    {
+      Icon: BsBookmarkCheck,
+      title: "ticketManagement",
+      description: "tickets",
+      link: "https://nea.org.np/ticket",
+    },
+    { Icon: FaAppStore, title: "NEAApp", description: "downloads", link: "#" },
+    { Icon: FaComment, title: "Complaints", description: "share", link: "#" },
+  ];
+
+  const SmallScreenCard = ({ Icon, title, description, link }) => (
+    <div className="bg-white rounded-lg shadow-xl p-6 flex items-center dark:bg-slate-700 dark:text-white">
+      <Icon
+        className="text-blue-500"
+        size={32}
+      />
+      <a href={link}>
+        <div className="ml-4">
+          <h3 className="text-lg font-semibold">{t(title)}</h3>
+          <p className="text-gray-600 dark:text-white">{t(description)}</p>
+        </div>
+      </a>
+    </div>
+  );
+
   return (
     <div className="flex items-center justify-center flex-col md:flex-row">
       {/* Cards for Small Screens */}
-      <div className="grid grid-cols-1 gap-2 md:hidden w-full p-4 ">
-        <div className="bg-white rounded-lg shadow-xl p-6 flex items-center  dark:bg-slate-700 dark:text-white">
-          <FaIdCard
-            className="text-blue-500"
-            size={32}
+      <div className="grid grid-cols-1 gap-2 md:hidden w-full p-4">
+        {smallScreenIcons.map((icon, index) => (
+          <SmallScreenCard
+            key={index}
+            {...icon}
           />
-          <div className="ml-4">
-            <h3 className="text-lg font-semibold">{t("attendance")}</h3>
-            <p className="text-gray-600  dark:text-white">
-              {t("manageAttendance")}
-            </p>
-          </div>
-        </div>
-        <div className="bg-white rounded-lg shadow-xl p-6 flex items-center  dark:bg-slate-700 dark:text-white">
-          <BsBookmarkCheck
-            className="text-blue-500"
-            size={32}
-          />
-          <div className="ml-4">
-            <h3 className="text-lg font-semibold">{t("ticketManagement")}</h3>
-            <p className="text-gray-600 dark:text-white">{t("tickets")}</p>
-          </div>
-        </div>
-        <div className="bg-white rounded-lg shadow-xl p-6 flex items-center  dark:bg-slate-700 dark:text-white">
-          <FaMoneyBill
-            className="text-blue-500"
-            size={32}
-          />
-          <div className="ml-4">
-            <h3 className="text-lg font-semibold ">{t("payroll")}</h3>
-            <p className="text-gray-600   dark:text-white">{t("Neapayroll")}</p>
-          </div>
-        </div>
-        <div className="bg-white rounded-lg shadow-xl p-6 flex items-center  dark:bg-slate-700 dark:text-white">
-          <FaAppStore
-            className="text-blue-500"
-            size={32}
-          />
-          <div className="ml-4">
-            <h3 className="text-lg font-semibold">{t("NEAApp")}</h3>
-            <p className="text-gray-600   dark:text-white">{t("downloads")}</p>
-          </div>
-        </div>
-        <div className="bg-white rounded-lg shadow-xl p-6 flex items-center  dark:bg-slate-700 dark:text-white">
-          <FaCarAlt
-            className="text-blue-500"
-            size={32}
-          />
-          <div className="ml-4">
-            <h3 className="text-lg font-semibold">{t("vehicle")}</h3>
-            <p className="text-gray-600   dark:text-white">
-              {t("vehicleManagement")}
-            </p>
-          </div>
-        </div>
-        <div className="bg-white rounded-lg shadow-xl p-6 flex items-center  dark:bg-slate-700 dark:text-white">
-          <FaComment
-            className="text-blue-500"
-            size={32}
-          />
-          <div className="ml-4">
-            <h3 className="text-lg font-semibold">{t("Complaints")}</h3>
-            <p className="text-gray-600  dark:text-white">{t("share")}</p>
-          </div>
-        </div>
+        ))}
       </div>
 
       {/* Existing Layout for Medium and Large Screens */}
       <div className="hidden md:flex flex-col items-center cursor-pointer ml-16 mt-16">
-        <div
-          className="w-24 h-24 md:w-28 md:h-28 bg-blue-700 hover:scale-105 hover:transition-all duration-300 ease-in-out flex items-center justify-center hover:bg-slate-700"
-          style={{
-            transform: "rotate(-45deg)",
-            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
-            borderRadius: "10px",
-            marginBottom: "-32px",
-            marginLeft: "4px",
-          }}
-        >
-          <div className="flex flex-col items-center justify-center w-full h-full">
-            <FaIdCard
-              className="text-white rotate-45 ml-7"
-              size={24}
-            />
-            <div className="transform text-center rotate-45 pr-4 text-slate-100 font-semibold mt-2">
-              <p>{t("attendance")}</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-row items-end justify-center w-full mt-2">
+        <a href="https://nea.org.np/hajiri">
+          {" "}
           <div
-            className="w-24 h-24 md:w-28 md:h-28 bg-green-700 hover:scale-105 hover:transition-all duration-300 ease-in-out flex items-center justify-center hover:bg-slate-700"
+            className="w-24 h-24 md:w-28 md:h-28 bg-blue-700 hover:scale-105 hover:transition-all duration-300 ease-in-out flex items-center justify-center hover:bg-slate-700"
             style={{
               transform: "rotate(-45deg)",
               boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
               borderRadius: "10px",
-              marginBottom: "10px",
-              marginRight: "50px",
+              marginBottom: "-32px",
+              marginLeft: "4px",
             }}
           >
             <div className="flex flex-col items-center justify-center w-full h-full">
-              <FaCarAlt
-                className="text-white ml-9 rotate-45"
-                size={32}
+              <FaIdCard
+                className="text-white rotate-45 ml-7"
+                size={24}
               />
-              <div className="transform rotate-45 pr-8 text-center text-slate-100 font-semibold mt-4">
-                <p>{t("vehicle")}</p>
+              <div className="transform text-center rotate-45 pr-4 text-slate-100 font-semibold mt-2">
+                <p>{t("attendance")}</p>
               </div>
             </div>
           </div>
+        </a>
+
+        <a href="https://nea.org.np/vehicle">
+          <div className="flex flex-row items-end justify-center w-full mt-2">
+            <div
+              className="w-24 h-24 md:w-28 md:h-28 bg-green-700 hover:scale-105 hover:transition-all duration-300 ease-in-out flex items-center justify-center hover:bg-slate-700"
+              style={{
+                transform: "rotate(-45deg)",
+                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
+                borderRadius: "10px",
+                marginBottom: "10px",
+                marginRight: "50px",
+              }}
+            >
+              <div className="flex flex-col items-center justify-center w-full h-full">
+                <FaCarAlt
+                  className="text-white ml-9 rotate-45"
+                  size={32}
+                />
+                <div className="transform rotate-45 pr-8 text-center text-slate-100 font-semibold mt-4">
+                  <p>{t("vehicle")}</p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="w-24 h-24 md:w-28 md:h-28 bg-purple-700 hover:scale-105 hover:transition-all duration-300 ease-in-out flex items-center justify-center hover:bg-slate-700"
+              style={{
+                transform: "rotate(-45deg)",
+                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
+                borderRadius: "10px",
+                marginBottom: "8px",
+                marginLeft: "10px",
+              }}
+            >
+              <div className="flex flex-col items-center justify-center w-full h-full">
+                <FaComment
+                  className="text-white rotate-45 ml-9"
+                  size={32}
+                />
+                <div className="transform rotate-45 pr-5 text-center text-slate-100 font-semibold mt-2">
+                  <p>{t("Complaints")}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </a>
+
+        <a href="https://nea.org.np/empPayroll">
           <div
-            className="w-24 h-24 md:w-28 md:h-28 bg-purple-700 hover:scale-105 hover:transition-all duration-300 ease-in-out flex items-center justify-center hover:bg-slate-700"
+            className="w-24 h-24 md:w-28 md:h-28 bg-orange-700 hover:scale-105 hover:transition-all duration-300 ease-in-out flex items-center justify-center hover:bg-slate-700 mt-2"
             style={{
               transform: "rotate(-45deg)",
               boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
               borderRadius: "10px",
-              marginBottom: "8px",
-              marginLeft: "10px",
+              marginTop: "-32px",
+              marginRight: "3px",
             }}
           >
             <div className="flex flex-col items-center justify-center w-full h-full">
-              <FaComment
-                className="text-white rotate-45 ml-9"
+              <FaMoneyBill
+                className="text-white rotate-45 ml-12"
                 size={32}
               />
-              <div className="transform rotate-45 pr-5 text-center text-slate-100 font-semibold mt-2">
-                <p>{t("Complaints")}</p>
+              <div className="transform rotate-45 pr-7 text-center pb-3 text-slate-100 font-semibold mt-4">
+                <p>{t("payroll")}</p>
               </div>
             </div>
           </div>
-        </div>
-
-        <div
-          className="w-24 h-24 md:w-28 md:h-28 bg-orange-700 hover:scale-105 hover:transition-all duration-300 ease-in-out flex items-center justify-center hover:bg-slate-700 mt-2"
-          style={{
-            transform: "rotate(-45deg)",
-            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
-            borderRadius: "10px",
-            marginTop: "-32px",
-            marginRight: "3px",
-          }}
-        >
-          <div className="flex flex-col items-center justify-center w-full h-full">
-            <FaMoneyBill
-              className="text-white rotate-45 ml-12"
-              size={32}
-            />
-            <div className="transform rotate-45 pr-7 text-center pb-3 text-slate-100 font-semibold mt-4">
-              <p>{t("payroll")}</p>
-            </div>
-          </div>
-        </div>
-
+        </a>
         {/* Additional Rhombus */}
         <div
           className="w-24 h-24 md:w-28 md:h-28 bg-red-700 hover:scale-105 hover:transition-all duration-300 ease-in-out flex items-center hover:bg-slate-700 justify-center mt-2"
@@ -180,18 +172,17 @@ const IconTile = () => {
             transform: "rotate(-45deg)",
             boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
             borderRadius: "10px",
-            marginTop: "-25px", // Adjust this value to position it correctly
+            marginTop: "-25px",
             marginRight: "180px",
           }}
         >
           <div className="flex flex-col items-center justify-center w-full h-full">
-            {/* Add your desired icon component */}
             <FaAppStore
               className="text-white rotate-45 ml-9"
               size={32}
             />
             <div className="transform rotate-45 pr-7 text-center text-slate-100 font-semibold mt-4">
-              <p>{t("NEAApp")}</p> {/* Change to your random name */}
+              <p>{t("NEAApp")}</p>
             </div>
           </div>
         </div>
@@ -203,18 +194,17 @@ const IconTile = () => {
             transform: "rotate(-45deg)",
             boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
             borderRadius: "10px",
-            marginTop: "-203px", // Adjust this value to position it correctly
+            marginTop: "-203px",
             marginLeft: "-340px",
           }}
         >
           <div className="flex flex-col items-center justify-center w-full h-full">
-            {/* Add your desired icon component */}
             <FaHandHoldingMedical
               className="text-white rotate-45 ml-9"
               size={32}
             />
             <div className="transform rotate-45 pr-7 text-center text-slate-100 font-semibold mt-4">
-              <p>{t("random")}</p> {/* Change to your random name */}
+              <p>{t("random")}</p>
             </div>
           </div>
         </div>
@@ -226,18 +216,17 @@ const IconTile = () => {
             transform: "rotate(-45deg)",
             boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
             borderRadius: "10px",
-            marginTop: "-20px", // Adjust this value to position it correctly
+            marginTop: "-20px",
             marginLeft: "172px",
           }}
         >
           <div className="flex flex-col items-center justify-center w-full h-full">
-            {/* Add your desired icon component */}
             <FaFeatherAlt
               className="text-white rotate-45 ml-9"
               size={32}
             />
             <div className="transform rotate-45 text-center pr-7 text-slate-100 font-semibold mt-4">
-              <p>{t("random")}</p> {/* Change to your random name */}
+              <p>{t("random")}</p>
             </div>
           </div>
         </div>
@@ -248,18 +237,17 @@ const IconTile = () => {
             transform: "rotate(-45deg)",
             boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
             borderRadius: "10px",
-            marginTop: "-200px", // Adjust this value to position it correctly
+            marginTop: "-200px",
             marginLeft: "345px",
           }}
         >
           <div className="flex flex-col items-center justify-center w-full h-full">
-            {/* Add your desired icon component */}
             <FaDove
               className="text-white rotate-45 ml-12"
               size={32}
             />
             <div className="transform rotate-45 pr-7 text-center text-slate-100 font-semibold mt-4">
-              <p>{t("random")}</p> {/* Change to your random name */}
+              <p>{t("random")}</p>
             </div>
           </div>
         </div>
@@ -270,19 +258,21 @@ const IconTile = () => {
             transform: "rotate(-45deg)",
             boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
             borderRadius: "10px",
-            marginTop: "65px", // Adjust this value to position it correctly
+            marginTop: "65px",
             marginLeft: "-5px",
           }}
         >
           <div className="flex flex-col items-center justify-center w-full h-full">
-            {/* Add your desired icon component */}
             <FaLaptop
               className="text-white rotate-45 ml-12"
               size={32}
             />
-            <div className="transform rotate-45 pr-7 text-center text-slate-100 font-semibold mt-4">
-              <p>{t("random")}</p> {/* Change to your random name */}
-            </div>
+            <a href="">
+              {" "}
+              <div className="transform rotate-45 pr-7 text-center text-slate-100 font-semibold mt-4">
+                <p>{t("random")}</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
